79590d1cfe4eb9690cd5a348e56aa708334d5978
Kirill Bystrov <kir9aza@gmail.com>
2022-11-08 00:02:22 +0300

    create page authorization

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $login = getUserLogin();
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Home page</title>
+	<title>Welcome page</title>
 </head>
 <body>
 	<?
---
+++ login.php
@@ -0,0 +1,37 @@
+<?
+if (!empty($_POST)) {
+	require __DIR__.'/checkAuth.php';
+	$login = $_POST['login'] ?? '';
+	$password = $_POST['password'] ?? '';
+	if (checkAuth($login,$password)){
+		setcookie('login',$login,0,'/');
+		setcookie('password',$password,0,'/');
+		header('Location: /index.php');
+	} else {
+		$error = 'Ошибка авторизации';
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Page authorization</title>
+</head>
+<body>
+	<? if (isset ($error)): ?>
+		<span style = 'color: red'> 
+			<?= $error?>
+		</span>
+	<? endif; ?>
+
+	<form action="login.php" method = 'post'>
+		<label for="login">Имя пользователя:</label>
+		<input type="text" name = 'login'>
+		<label for="password">Пароль:</label>
+		<input type="text" name = 'password'>
+		<input type="submit">
+	</form>
+</body>
+</html>
