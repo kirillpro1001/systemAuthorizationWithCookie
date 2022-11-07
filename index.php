1695d65d7afbaa7ffb5b365543ac519c3421664d
Kirill Bystrov <kir9aza@gmail.com>
2022-11-07 23:51:29 +0300

    create welcome page

---
+++ index.php
@@ -0,0 +1,25 @@
+<?
+require __DIR__.'/checkAuth.php';
+$login = getUserLogin();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home page</title>
+</head>
+<body>
+	<?
+	if ($login === null):
+	 ?>
+	 <a href="login.php">Авторизуйтесь</a>
+	<? else: ?>
+		Добро пожаловать, <?= $login ?>
+		<br>
+	<a href="logout.php">Выйти</a>
+<? endif; ?>
+</body>
+</html>
